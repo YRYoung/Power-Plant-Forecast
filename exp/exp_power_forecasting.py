50905484c664d9743a17fd526926d834599470fb
Mechrevo <mechrevo@example.com>
2023-06-18 22:57:52 +0100

    implemented training on GPU

--- exp/exp_power_forecasting.py
+++ exp/exp_power_forecasting.py
@@ -21,6 +21,29 @@ class ExpPowerForecast():
         self.writer = None
         self.provider = custom_data_provider(self.args)
 
+        device_name = 'cuda' if args.use_gpu else 'cpu'
+        self.device = torch.device(device_name)
+        self.model = TimesNet.Model(self.args).float()
+
+        path = os.path.join(self.args.checkpoints, self.args.session_id)
+
+        os.makedirs(path, exist_ok=True)
+
+        self.best_model_path = os.path.join(path, 'checkpoint.pth')
+        trained = os.path.exists(self.best_model_path)
+        val_loss = np.Inf
+        print(f'{"Trained" if trained else "New"} model loaded on {device_name}')
+        if trained:
+            checkpoint = torch.load(self.best_model_path, map_location=self.device)
+            self.model.load_state_dict(checkpoint['model'])
+            val_loss = checkpoint['val_loss']
+            print(f'Previous minimum val_loss: {val_loss:.6f}')
+
+        self.early_stopping = EarlyStopping(save_path=self.best_model_path, val_loss_min=val_loss,
+                                            patience=self.args.patience, verbose=True)
+
+        self.model.to(self.device)
+
     def _get_data(self, flag):
         returns = {}
         if flag not in returns:
@@ -39,23 +62,21 @@ class ExpPowerForecast():
         self.model.eval()
         with torch.no_grad():
             for i, (batch_x, batch_y, batch_x_mark, _) in enumerate(vali_loader):
-                batch_x = batch_x.float().to(self.device)
-                batch_y = batch_y.float()
+                if self.args.use_gpu:
+                    batch_x = batch_x.cuda(non_blocking=True)
+                    batch_y = batch_y.cuda(non_blocking=True)
+                    batch_x_mark = batch_x_mark.cuda(non_blocking=True)
 
-                batch_x_mark = batch_x_mark.float().to(self.device)
+                with torch.cuda.amp.autocast(self.args.use_amp):
+                    outputs = self.model(batch_x, batch_x_mark)
 
-                outputs = self.model(batch_x, batch_x_mark)
+                    f_dim = -1 if self.args.features == 'MS' else 0
+                    outputs = outputs[:, :, f_dim:]
+                    batch_y = batch_y[:, :, f_dim:]
 
-                f_dim = -1 if self.args.features == 'MS' else 0
-                outputs = outputs[:, :, f_dim:]
-                batch_y = batch_y[:, :, f_dim:]
+                    loss = criterion(outputs, batch_y)
 
-                pred = outputs.detach().cpu()
-                true = batch_y.detach().cpu()
-
-                loss = criterion(pred, true)
-
-                total_loss.append(loss)
+                total_loss[i] = loss.item()
         total_loss = np.average(total_loss)
         self.model.train()
         return total_loss
@@ -66,14 +87,7 @@ class ExpPowerForecast():
         vali_data, vali_loader = self._get_data(flag='val')
         test_data, test_loader = self._get_data(flag='test')
 
-        path = os.path.join(self.args.checkpoints, setting)
-        if not os.path.exists(path):
-            os.makedirs(path)
-
         time_now = time.time()
-
-        train_steps = len(train_loader)
-        early_stopping = EarlyStopping(patience=self.args.patience, verbose=True)
 
         model_optim = self._select_optimizer()
         criterion = self._select_criterion()
@@ -99,18 +113,20 @@ class ExpPowerForecast():
             for i, (batch_x, batch_y, batch_x_mark, _) in enumerate(train_loader):
                 iter_count += 1
                 model_optim.zero_grad()
+                if self.args.use_gpu:
+                    batch_x = batch_x.cuda(non_blocking=True)
+                    batch_y = batch_y.cuda(non_blocking=True)
+                    batch_x_mark = batch_x_mark.cuda(non_blocking=True)
 
-                batch_x = batch_x.float().to(self.device)
-                batch_y = batch_y.float().to(self.device)
-                batch_x_mark = batch_x_mark.float().to(self.device)
+                with torch.cuda.amp.autocast(self.args.use_amp):
 
-                outputs = self.model(batch_x, batch_x_mark)
+                    outputs = self.model(batch_x, batch_x_mark)
+                    f_dim = -1 if self.args.features == 'MS' else 0
+                    outputs = outputs[:, :, f_dim:]
+                    batch_y = batch_y[:, :, f_dim:]
+                    loss = criterion(outputs, batch_y)
 
-                f_dim = -1 if self.args.features == 'MS' else 0
-                outputs = outputs[:, :, f_dim:]
-                batch_y = batch_y[:, :, f_dim:]
-                loss = criterion(outputs, batch_y)
-                train_loss.append(loss.item())
+                train_loss[i] = loss.item()
 
                 if (i + 1) % 10 == 0:
                     self.writer['train/batch_loss'].append(loss.item())

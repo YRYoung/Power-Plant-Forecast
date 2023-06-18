9e4be151bc19165047e577ed7de90a46a977596a
Mechrevo <mechrevo@example.com>
2023-06-18 22:55:51 +0100

    improved logging, save_model within early stopping

--- utils/tools.py
+++ utils/tools.py
@@ -29,31 +29,29 @@ def adjust_learning_rate(optimizer, epoch, args):
 
 
 class EarlyStopping:
-    def __init__(self, patience=7, verbose=False, delta=0):
+    def __init__(self, save_path, patience=7, verbose=False, tolerance=0, val_loss_min=np.Inf):
+        self.save_path = save_path
         self.patience = patience
         self.verbose = verbose
         self.counter = 0
-        self.best_score = None
-        self.early_stop = False
-        self.val_loss_min = np.Inf
-        self.delta = delta
 
-    def __call__(self, val_loss, model, path):
-        score = -val_loss
-        if self.best_score is None:
-            self.best_score = score
-            self.save_checkpoint(val_loss, model, path)
-        elif score < self.best_score + self.delta:
+        self.stop = False
+        self.val_loss_min = val_loss_min
+        self.tolerance = tolerance
+
+    def __call__(self, val_loss, model, prefix):
+
+        if val_loss > self.val_loss_min + self.tolerance:
             self.counter += 1
             print(prefix + f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
-                self.early_stop = True
+                self.stop = True
         else:
-            self.best_score = score
-            self.save_checkpoint(val_loss, model, path)
+
+            self.save_checkpoint(val_loss, model, prefix)
             self.counter = 0
 
-    def save_checkpoint(self, val_loss, model, path):
+    def save_checkpoint(self, val_loss, model, prefix):
         if self.verbose:
             print(prefix + f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}). Saving model ...')
         torch.save({
